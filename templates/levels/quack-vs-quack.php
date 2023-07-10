24ff6aef7bf94013bdb65fdefe868f3a86251d96
mentosmenno2 <mentosmenno2@hotmail.com>
2023-07-10 23:40:35 +0200

    progress bar css

--- templates/levels/quack-vs-quack.php
+++ templates/levels/quack-vs-quack.php
@@ -7,8 +7,13 @@ use Mentosmenno2\MaartenBday2023\Levels\AbstractLevel;
 ?>
 
 <div class="level level-quack-vs-quack">
-	<div class="healthbar-wrapper">
-		<div class="healthbar-progress"></div>
+	<div class="healthbar-wrapper healthbar-wrapper-player">
+		<div class="healthbar-progress healthbar-progress-player"></div>
+		<span class="healthbar-text">Your health</span>
+	</div>
+
+	<div class="healthbar-wrapper healthbar-wrapper-enemy">
+		<div class="healthbar-progress healthbar-wrapper-progress"></div>
 		<span class="healthbar-text">Evil duck health</span>
 	</div>
 
