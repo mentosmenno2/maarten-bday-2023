1771a3e2c6b032e6dce7c8f37b44f4f2db57b705
mentosmenno2 <mentosmenno2@hotmail.com>
2023-07-12 23:25:12 +0200

    bigger progress bars

--- templates/levels/tag-a-duck.php
+++ templates/levels/tag-a-duck.php
@@ -9,7 +9,7 @@ use Mentosmenno2\MaartenBday2023\Levels\AbstractLevel;
 <div class="level level-tag-a-duck">
 	<div class="progressbar-wrapper">
 		<div class="progressbar-progress"></div>
-		<span class="progressbar-text">Times tagged</span>
+		<span class="progressbar-text">Tags</span>
 	</div>
 
 	<div class="character character-player">
