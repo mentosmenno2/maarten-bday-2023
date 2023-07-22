3999d0eed1d25d4d783fafb2fb4ca46b85c29ea2
mentosmenno2 <mentosmenno2@hotmail.com>
2023-07-23 01:40:01 +0200

    fix progress bar texts mutliplayer quack vs quack

--- templates/levels/quack-vs-quack.php
+++ templates/levels/quack-vs-quack.php
@@ -11,12 +11,24 @@ use Mentosmenno2\MaartenBday2023\Levels\AbstractLevel;
 <div class="level level-quack-vs-quack">
 	<div class="healthbar-wrapper healthbar-wrapper-player">
 		<div class="healthbar-progress healthbar-progress-player"></div>
-		<span class="healthbar-text">Your health</span>
+		<span class="healthbar-text">
+			<?php if ($game->getPlayers() > 1) { ?>
+				Player 1 health
+			<?php } else { ?>
+				Your health
+			<?php } ?>
+		</span>
 	</div>
 
 	<div class="healthbar-wrapper healthbar-wrapper-enemy">
 		<div class="healthbar-progress healthbar-progress-enemy"></div>
-		<span class="healthbar-text">Evil duck health</span>
+		<span class="healthbar-text">
+			<?php if ($game->getPlayers() > 1) { ?>
+				Player 2 health
+			<?php } else { ?>
+				Evil duck health
+			<?php } ?>
+		</span>
 	</div>
 
 	<div class="character character-enemy">
