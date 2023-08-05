4bda5c2db65b52a5c43599f454ff3a244e53a819
mentosmenno2 <mentosmenno2@hotmail.com>
2023-08-05 19:11:22 +0200

    add button sound effects

--- templates/levels/start.php
+++ templates/levels/start.php
@@ -13,8 +13,8 @@ use Mentosmenno2\MaartenBday2023\Levels\AbstractLevel;
 	<div class="setting-container setting-container-mode">
 		<h2>What do you want to play?</h2>
 		<div class="setting-choices setting-minigame-choices" >
-			<button class="button button-setting-mode" data-mode="story" >Story mode</button>
-			<button class="button button-setting-mode" data-mode="minigame" >Minigame</button>
+			<button class="button button-sfx button-setting-mode" data-mode="story" >Story mode</button>
+			<button class="button button-sfx button-setting-mode" data-mode="minigame" >Minigame</button>
 		</div>
 	</div>
 
@@ -22,7 +22,7 @@ use Mentosmenno2\MaartenBday2023\Levels\AbstractLevel;
 		<h2>Select a minigame</h2>
 		<div class="setting-choices setting-minigame-choices" >
 			<?php foreach ($game->getLevelSelectOptions() as $id => $label) { ?>
-				<button class="button button-setting-minigame" data-level="<?php echo $id; ?>"><?php echo $label; ?></button>
+				<button class="button button-sfx button-setting-minigame" data-level="<?php echo $id; ?>"><?php echo $label; ?></button>
 			<?php } ?>
 		</div>
 	</div>
@@ -30,8 +30,8 @@ use Mentosmenno2\MaartenBday2023\Levels\AbstractLevel;
 	<div class="setting-container setting-container-players">
 		<h2>With how many players do you want to play?</h2>
 		<div class="setting-choices setting-minigame-choices" >
-			<button class="button button-setting-players" data-players="1" >1 player</button>
-			<button class="button button-setting-players" data-players="2" >2 players (requires keyboard)</button>
+			<button class="button button-sfx button-setting-players" data-players="1" >1 player</button>
+			<button class="button button-sfx button-setting-players" data-players="2" >2 players (requires keyboard)</button>
 		</div>
 	</div>
 </div>
--- templates/results.php
+++ templates/results.php
@@ -7,21 +7,21 @@
 <div class="results">
 	<div class="results-option results-option-won">
 		<h2>You win</h2>
-		<button class="button button-results button-results-won">Continue</button>
+		<button class="button button-sfx button-results button-results-won">Continue</button>
 	</div>
 
 	<div class="results-option results-option-lost">
 		<h2>You lose</h2>
-		<button class="button button-results button-results-lost">Retry</button>
+		<button class="button button-sfx button-results button-results-lost">Retry</button>
 	</div>
 
 	<div class="results-option results-option-won-mp">
 		<h2>Player <span class="results-mp-player-number">1</span> wins</h2>
-		<button class="button button-results button-results-won">Continue</button>
+		<button class="button button-sfx button-results button-results-won">Continue</button>
 	</div>
 
 	<div class="results-option results-option-draw">
 		<h2>Draw</h2>
-		<button class="button button-results button-results-draw">Continue</button>
+		<button class="button button-sfx button-results button-results-draw">Continue</button>
 	</div>
 </div>
