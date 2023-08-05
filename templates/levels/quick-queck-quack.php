8bb7dffadfe3b9e1ca72351fc7495d834a8999c5
mentosmenno2 <mentosmenno2@hotmail.com>
2023-08-05 15:25:36 +0200

    add css and html

--- templates/levels/quick-queck-quack.php
+++ templates/levels/quick-queck-quack.php
@@ -9,5 +9,10 @@ use Mentosmenno2\MaartenBday2023\Levels\AbstractLevel;
 ?>
 
 <div class="level level-quick-queck-quack">
-
+	<div class="turn-text"><span class="turn-indicator">Your</span> turn</div>
+	<div class="buttons-container">
+		<?php for ($i=0; $i < 9; $i++) { ?>
+			<button class="button button-spot" data-spot="<?php echo $i + 1; ?>"></button>
+		<?php } ?>
+	</div>
 </div>
